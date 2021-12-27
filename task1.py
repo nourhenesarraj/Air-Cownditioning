aa5af6c5820fd0f3cb92c81c38fcefbf6e1fab3f
Nourhéne Sarraj <sarrajnourhene1@gmail.com>
2021-12-27 14:38:29 +0100

    first commit

---
+++ task1.py
@@ -0,0 +1,11 @@
+while True:
+    N = int(input("Enter number of stalles: "))
+    if N < 0:
+        print("Number must be positive!")
+        continue
+    else:
+        break 
+P = [int(P) for P in input("Enter P Values: ").split(" ")]
+T = [int(T) for T in input("Enter T Values: ").split(" ")]
+diff = [x-y for x,y in zip(P,T)]  
+print("Output: ",sum(abs(x-y) for x,y in zip(diff+[0],[0]+diff))//2)
